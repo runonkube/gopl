69783398a8870f2936f988fc3e0d9ed1b0638584
olushiyanbade <olushiyanbade@runcloudnative.com>
2026-06-29 11:17:02 +0100

    cmd tool for creating/viewing/closing gh issues

--- ch4/github/github.go
+++ ch4/github/github.go
@@ -4,8 +4,15 @@
 package github
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
 	"time"
 )
+
+const issuesBaseUrl = "https://api.github.com"
 
 const IssuesURL = "https://api.github.com/search/issues"
 
@@ -27,4 +34,41 @@ type Issue struct {
 type User struct {
 	Login   string
 	HTMLURL string `json:"html_url"`
+}
+
+type IssueCreateRequest struct {
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+type IssueClient struct {
+	http http.Client
+}
+
+func (client *IssueClient) Create(ownerRepo string, payload *IssueCreateRequest) (*Issue, error) {
+
+	reqBody, err := json.Marshal(*payload)
+	if err != nil {
+		return nil, fmt.Errorf("Error marshalling request payload: %s", err)
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/repos/%s/issues", issuesBaseUrl, ownerRepo), bytes.NewReader(reqBody))
+	if err != nil {
+		return nil, fmt.Errorf("Error when creating request:%s", err)
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("GITHUB_TOKEN")))
+
+	resp, err := client.http.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("Error when creating issue:%s", err)
+	}
+	defer resp.Body.Close()
+
+	result := Issue{}
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return &result, nil
 }
