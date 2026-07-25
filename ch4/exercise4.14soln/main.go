3cc0617d37b0b85fd7c9b1ca7ffabb28e220fa05
olushiyanbade <olushiyanbade@runcloudnative.com>
2026-07-25 23:44:45 +0100

    templates exercise

--- ch4/exercise4.14soln/main.go
+++ ch4/exercise4.14soln/main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/runonkube/issues-cli/pkg/github"
+)
+
+var (
+	client          = github.NewIssueClient(nil)
+	displayTemplate = template.Must(template.New("issuelist").Parse(`
+<h1>{{. | len}} issues</h1>
+<table>
+<tr style='text-align: left'>
+  <th>#</th>
+  <th>State</th>
+  <th>User</th>
+  <th>Title</th>
+</tr>
+{{range .}}
+<tr>
+  <td><a href='{{.HTMLURL}}'>{{.Number}}</a></td>
+  <td>{{.State}}</td>
+  <td><a href='{{.User.HTMLURL}}'>{{.User.Login}}</a></td>
+  <td><a href='{{.HTMLURL}}'>{{.Title}}</a></td>
+</tr>
+{{end}}
+</table>`))
 )
 
 func main() {
@@ -13,5 +38,25 @@ func main() {
 
 func GetIssues(responseWriter http.ResponseWriter, request *http.Request) {
 
-	fmt.Fprint(responseWriter, "HelloWorld")
+	queryParams := request.URL.Query()
+	repo := queryParams.Get("repo")
+	state := queryParams.Get("state")
+
+	if repo == "" || state == "" {
+		fmt.Fprintln(responseWriter, "Must specify repo and state e.g. 'http://localhost:8080/issues?repo=runonkube/issues-cli&state=open'")
+		return
+	}
+
+	issues, err := client.ListIssues(repo, state)
+
+	if err != nil {
+		fmt.Fprintf(responseWriter, "Error: %s", err)
+		return
+	}
+
+	responseWriter.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if err := displayTemplate.Execute(responseWriter, issues); err != nil {
+		fmt.Fprintf(responseWriter, "Error: %s", err)
+	}
+
 }
